3b5b1b33637eb50deca5981008fe476054d519ab
Marek Szwalkiewicz <marek@szwalkiewicz.waw.pl>
2014-01-04 17:31:36 +0100

    Connected simple client movement to server

--- client/run.py
+++ client/run.py
@@ -1,67 +1,87 @@
 import curses
 
-players_checked = 0
+import requests
 
 
 def get_players():
+    return requests.get('http://localhost:8888/players.json').json()
 
-    # mocked for now
-    global players_checked
 
-    players_checked = players_checked + 1
+def new_player():
+    return requests.post(
+        'http://localhost:8888/players.json',
+        data={'name': 'Marek'}
+    ).json()['uid']
 
-    result = [
-        (10, 12, "Jonasz", 1),
-        (15, 32, "Augiasz", 2),
-        (2+(players_checked/10), 2+(players_checked/10), "RUN", 3)
-    ]
 
-    if players_checked > 100:
-        result.append(
-            (13,
-             players_checked/10,
-             "Nowosz",
-             4)
-        )
+def update_player(uid, y, x):
+    requests.put(
+        'http://localhost:8888/players.json',
+        data={
+            'y': y,
+            'x': x,
+            'uid': uid
+        }
+    )
 
-    return result
+
+def remove_player(uid):
+    requests.delete(
+        'http://localhost:8888/players.json',
+        params={'uid':uid}
+    )
 
 
 def main_loop(stdscr):
-    global players_checked
-
     curses.curs_set(0)
+    stdscr.nodelay(1)
     player_char = curses.ACS_DIAMOND
-    new_player = curses.A_STANDOUT
-    old_player = curses.A_DIM
+    stdscr_y, stdscr_x = stdscr.getmaxyx()
 
-    seen_players_uids = []
+    our_uid = new_player()
 
-    while(players_checked < 300): # I know, ok?
+    try:
+        while(True): # I know, ok?
 
-        # Clearing screen from last "frame"
-        stdscr.clear()
+            # Clearing screen from last "frame"
+            stdscr.clear()
 
-        players = get_players()
+            players = get_players()
 
-        for player in players:
-            y, x, name, uid = player
+            for player in players['players']:
+                y = int(player['y'])
+                x = int(player['x'])
+                name = player['name']
+                uid = player['id']
 
-            # Adding name of the player above their head
-            stdscr.addstr(y-1, x-(len(name)/2), name)
+                # Adding name of the player above their head
+                stdscr.addstr(y-1, x-(len(name)/2), name)
 
-            # Adding player box (new players shine)
-            if uid in seen_players_uids:
-                stdscr.addch(y, x, player_char, old_player)
-            else:
-                stdscr.addch(y, x, player_char, new_player)
-                seen_players_uids.append(uid)
+                # Adding player box (new players shine)
+                stdscr.addch(y, x, player_char)
+                stdscr.addstr(0,0,uid)
+                stdscr.addstr(0,0,our_uid)
+                # --- OUR PLAYER PART ---
+                if uid == our_uid:
 
-        # "frame" number for debugging
-        stdscr.addstr(0,0, str(players_checked))
+                    key_pressed = stdscr.getch()
+                    # We react on movement
+                    if key_pressed == curses.KEY_UP and y > 0:
+                        y -= 1
+                    elif key_pressed == curses.KEY_DOWN and y < stdscr_y-1:
+                        y += 1
+                    elif key_pressed == curses.KEY_LEFT and x > 0:
+                        x -= 1
+                    elif key_pressed == curses.KEY_RIGHT and x < stdscr_x-1:
+                        x += 1
 
-        stdscr.refresh()
-        curses.napms(100)
+                    # and send movement update to server
+                    update_player(uid, y, x)
+
+            stdscr.refresh()
+            curses.napms(500)
+    except KeyboardInterrupt:
+        remove_player(our_uid)
 
 
 if __name__ == '__main__':
